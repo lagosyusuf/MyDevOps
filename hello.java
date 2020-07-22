e8c8ce9b8f6e3388698e8a6f30537f8f307219e4
lagos yusuf <lagosyusuf@gmail.com>
2020-07-22 21:37:07 +0000

    added a new print statement

--- hello.java
+++ hello.java
@@ -2,6 +2,9 @@
 
 class Hello{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java");
+    
+     System.out.println("Welcome to Srint Boot");
+
     }  
 }  
